55b4e5a5994808e1e09ee379c4639b01f03927a2
ShankarKushwah <shankarkushwah94@gmail.com>
2019-03-06 02:03:33 +0530

    created tables done in models.

--- ieju/juapp/admin.py
+++ ieju/juapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Branch, Semester, Syllabus
 
-# Register your models here.
+
+class BranchAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+class SemesterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'branch', )
+
+
+class SyllabusAdmin(admin.ModelAdmin):
+    list_display = ('name', 'branch', 'semester', )
+
+
+admin.site.register(Branch, BranchAdmin)
+admin.site.register(Semester, SemesterAdmin)
+admin.site.register(Syllabus, SyllabusAdmin)
--- ieju/juapp/models.py
+++ ieju/juapp/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 
 
+class Branch(models.Model):
+    name = models.CharField(max_length=60, db_column='branch_name')
+
+    def __str__(self):
+        return self.name
+
+
 class Semester(models.Model):
-	pass
+    name = models.CharField(max_length=60, db_column='semester_name')
+    branch = models.ForeignKey(Branch)
+
+    def __str__(self):
+        return self.branch
 
 
 class Syllabus(models.Model):
-	pass
-
-
-class Exam(models.Model):
-	pass
-
+    name = models.CharField(max_length=60, db_column='syllabus_name')
+    branch = models.ForeignKey(Branch)
+    semester = models.ForeignKey(Semester)
